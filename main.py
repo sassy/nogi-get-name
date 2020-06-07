75ac4bff435450c53fa3a64d43801bbf1ee9606e
Satoshi Watanabe <sassy.watanabe@gmail.com>
2020-06-07 14:25:30 +0900

    support kana and birthday

--- main.py
+++ main.py
@@ -1,15 +1,41 @@
 import urllib.request
+from urllib.parse import urljoin
 from bs4 import BeautifulSoup
 
-def main():
-    url = 'http://www.nogizaka46.com/member/'
-    req = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
-    html = urllib.request.urlopen(req).read()
+base_url = 'http://www.nogizaka46.com/member/'
+
+def get_html():
+    req = urllib.request.Request(base_url, headers={'User-Agent': 'Mozilla/5.0'})
+    return urllib.request.urlopen(req).read()
+
+def namelist():
+    html = get_html()
     soup = BeautifulSoup(html, "html.parser")
     units = soup.find_all("div", {"class": "unit"})
     for unit in units:
         if unit.find('span',  {"class": "main"}) is not None:
-            print(unit.find('span',  {"class": "main"}).string)
+            kanji = unit.find('span',  {"class": "main"}).string
+            kana = unit.find('span',  {"class": "sub"}).string if unit.find('span',  {"class": "sub"}) is not None else unit.find('span',  {"class": "sub2"}).string
+            print(kanji + " " + kana )
+
+def namelist_with_birthday():
+    html = get_html()
+    soup = BeautifulSoup(html, "html.parser")
+    units = soup.find_all("div", {"class": "unit"})
+    for unit in units:
+        if unit.find('span',  {"class": "main"}) is not None:
+            kanji = unit.find('span',  {"class": "main"}).string
+            kana = unit.find('span',  {"class": "sub"}).string if unit.find('span',  {"class": "sub"}) is not None else unit.find('span',  {"class": "sub2"}).string
+            url = urljoin(base_url, unit.find('a')['href'])
+            req = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+            next_html = urllib.request.urlopen(req).read()
+            soup2 = BeautifulSoup(next_html, "html.parser")
+            birthday = soup2.find_all("dd")[0].string
+            print(kanji + " " + kana + " " + birthday)
+
+def main():
+    #namelist()
+    namelist_with_birthday()
 
 if __name__ == '__main__':
     main()
